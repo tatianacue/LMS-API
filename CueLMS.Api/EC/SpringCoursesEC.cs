417d9338df5c8c7d7715236f6c20b0a8c11a5b21
tatcue <tatcu@ATIAN>
2023-04-20 15:25:53 -0400

    added spring course controller

---
+++ CueLMS.Api/EC/SpringCoursesEC.cs
@@ -0,0 +1,33 @@
+﻿using Library.LMS.Models;
+using UWP.Library.CueLMS.Database;
+
+namespace CueLMS.Api.EC
+{
+    public class SpringCoursesEC
+    {
+        public List<Course> GetCourses()
+        {
+            return FakeDatabaseContext.SpringCourses;
+        }
+
+        public Course AddOrUpdateCourse(Course c)
+        {
+            if (c.Id > 0)
+            {
+                var itemToUpdate = FakeDatabaseContext.SpringCourses.FirstOrDefault(x => x.Id == c.Id);
+                if (itemToUpdate != null)
+                {
+                    FakeDatabaseContext.SpringCourses.Remove(itemToUpdate);
+                    FakeDatabaseContext.SpringCourses.Add(c);
+                }
+            }
+            else
+            {
+                var lastId = FakeDatabaseContext.SpringCourses.Select(x => x.Id).Max();
+                c.Id = ++lastId;
+                FakeDatabaseContext.SpringCourses.Add(c);
+            }
+            return c;
+        }
+    }
+}
