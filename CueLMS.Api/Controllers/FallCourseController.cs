cd8bb1c81066d85fce44a3d4a73a6e7af30a7a65
tatcue <tatcu@ATIAN>
2023-04-20 16:29:24 -0400

    added controllers for both fall and spring courses

---
+++ CueLMS.Api/Controllers/FallCourseController.cs
@@ -0,0 +1,30 @@
+﻿using CueLMS.Api.EC;
+using Library.LMS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CueLMS.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FallCourseController
+    {
+        private readonly ILogger<FallCourseController> _logger;
+
+        public FallCourseController(ILogger<FallCourseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Course> GetCourses()
+        {
+            return new FallCoursesEC().GetCourses();
+        }
+
+        [HttpPost]
+        public Course AddOrUpdate([FromBody] Course course)
+        {
+            return new FallCoursesEC().AddOrUpdateCourse(course);
+        }
+    }
+}
---
+++ CueLMS.Api/EC/SummerCoursesEC.cs
@@ -0,0 +1,33 @@
+﻿using Library.LMS.Models;
+using UWP.Library.CueLMS.Database;
+
+namespace CueLMS.Api.EC
+{
+    public class SummerCoursesEC
+    {
+        public List<Course> GetCourses()
+        {
+            return FakeDatabaseContext.SummerCourses;
+        }
+
+        public Course AddOrUpdateCourse(Course c)
+        {
+            if (c.Id > 0)
+            {
+                var itemToUpdate = FakeDatabaseContext.SummerCourses.FirstOrDefault(x => x.Id == c.Id);
+                if (itemToUpdate != null)
+                {
+                    FakeDatabaseContext.SummerCourses.Remove(itemToUpdate);
+                    FakeDatabaseContext.SummerCourses.Add(c);
+                }
+            }
+            else
+            {
+                var lastId = FakeDatabaseContext.SummerCourses.Select(x => x.Id).Max();
+                c.Id = ++lastId;
+                FakeDatabaseContext.SummerCourses.Add(c);
+            }
+            return c;
+        }
+    }
+}
