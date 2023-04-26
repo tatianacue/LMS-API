8bafc460ba149d497997cd934352d754496f58d5
tatianacue <tbc20n@fsu.edu>
2023-04-26 12:59:15 -0400

    added module controllers

---
+++ CueLMS.Api/EC/ModuleEC.cs
@@ -0,0 +1,90 @@
+﻿using Library.LMS.Models;
+using UWP.Library.CueLMS.Database;
+
+namespace CueLMS.Api.EC
+{
+    public class ModuleEC
+    {
+        public List<Module> GetModules(int id)
+        {
+            var Course = FakeDatabaseContext.SpringCourses.FirstOrDefault(x => x.Id == id);
+            if (Course == null)
+            {
+                Course = FakeDatabaseContext.FallCourses.FirstOrDefault(x => x.Id == id);
+            }
+            if (Course == null)
+            {
+                Course = FakeDatabaseContext.SummerCourses.FirstOrDefault(x => x.Id == id);
+            }
+            if (Course != null)
+            {
+                return Course.Modules;
+            }
+            else
+            {
+                return new List<Module>();
+            }
+        }
+
+        public void AddOrUpdateModule(Course c)
+        {
+            var Course = FakeDatabaseContext.SpringCourses.FirstOrDefault(x => x.Id == c.Id);
+            if (Course == null)
+            {
+                Course = FakeDatabaseContext.FallCourses.FirstOrDefault(x => x.Id == c.Id);
+            }
+            if (Course == null)
+            {
+                Course = FakeDatabaseContext.SummerCourses.FirstOrDefault(x => x.Id == c.Id);
+            }
+            if (Course != null)
+            {
+                var Module = c.SelectedModule;
+                if (Module.Id > 0)
+                {
+                    var itemToUpdate = Course.Modules.FirstOrDefault(x => x.Id == Module.Id);
+                    if (itemToUpdate != null)
+                    {
+                        Course.Modules.Remove(itemToUpdate);
+                        Course.Modules.Add(Module);
+                    }
+                }
+                else
+                {
+                    int lastId;
+                    if (Course.Modules.Count > 0)
+                    {
+                        lastId = Course.Modules.Select(x => x.Id).Max();
+                    }
+                    else
+                    {
+                        lastId = 0;
+                    }
+                    Module.Id = ++lastId;
+                    Course.Modules.Add(Module);
+                }
+            }
+        }
+        public void DeleteModule(Course c)
+        {
+            var Course = FakeDatabaseContext.SpringCourses.FirstOrDefault(x => x.Id == c.Id);
+            if (Course == null)
+            {
+                Course = FakeDatabaseContext.FallCourses.FirstOrDefault(x => x.Id == c.Id);
+            }
+            if (Course == null)
+            {
+                Course = FakeDatabaseContext.SummerCourses.FirstOrDefault(x => x.Id == c.Id);
+            }
+            if (Course != null)
+            {
+                var Module = c.SelectedModule;
+                var item = Course.Modules.FirstOrDefault(x => x.Id == Module.Id);
+                if (item != null)
+                {
+                    Course.Modules.Remove(item);
+                }
+            }
+        }
+    }
+}
