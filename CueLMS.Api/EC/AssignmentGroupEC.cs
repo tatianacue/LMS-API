043022d5563637e03e1b3812b88311bb14055305
tatianacue <tbc20n@fsu.edu>
2023-04-27 00:57:43 -0400

    created submission controllers and allows assignment group to be able to send assignments

--- CueLMS.Api/EC/AssignmentGroupEC.cs
+++ CueLMS.Api/EC/AssignmentGroupEC.cs
@@ -48,9 +48,9 @@ namespace CueLMS.Api.EC
                 else //add assignment group
                 {
                     int lastId;
-                    if (Course.AssignmentGroups.Count > 0)
+                    if (FakeDatabaseContext.AssignmentGroupIds.Count > 0)
                     {
-                        lastId = Course.AssignmentGroups.Select(x => x.Id).Max();
+                        lastId = FakeDatabaseContext.AssignmentGroupIds.Max();
                     }
                     else
                     {
@@ -59,7 +59,60 @@ namespace CueLMS.Api.EC
                     g.Id = ++lastId;
                     g.Group.Add(c.SelectedAssignment); //adds assignment to group
                     Course.AssignmentGroups.Add(g); //adds group to group list
+                    FakeDatabaseContext.AssignmentGroupIds.Add(g.Id); //adds id to used id list
                 }
+            }
+        }
+        public List<Assignment> GetAssignments(int id)
+        {
+            AssignmentGroup getfrom = new AssignmentGroup();
+            int found = 0; //finding what course and group the list is from
+            foreach (var course in FakeDatabaseContext.SpringCourses)
+            {
+                foreach (var group in course.AssignmentGroups)
+                {
+                    if (group.Id == id)
+                    {
+                        getfrom = group;
+                        found = 1; break;
+                    }
+                }
+            }
+            if (found == 0)
+            {
+                foreach (var course in FakeDatabaseContext.SummerCourses)
+                {
+                    foreach (var group in course.AssignmentGroups)
+                    {
+                        if (group.Id == id)
+                        {
+                            getfrom = group;
+                            found = 1; break;
+                        }
+                    }
+                }
+            }
+            if (found == 0)
+            {
+                foreach (var course in FakeDatabaseContext.FallCourses)
+                {
+                    foreach (var group in course.AssignmentGroups)
+                    {
+                        if (group.Id == id)
+                        {
+                            getfrom = group;
+                            found = 1; break;
+                        }
+                    }
+                }
+            }
+            if (found == 1)
+            {
+                return getfrom.Group; //list of assignments
+            }
+            else
+            {
+                return new List<Assignment>();
             }
         }
     }
---
+++ CueLMS.Api/EC/SubmissionEC.cs
@@ -0,0 +1,69 @@
+﻿using Library.LMS.Models;
+using Library.LMS.Models.Grading;
+using UWP.Library.CueLMS.Database;
+
+namespace CueLMS.Api.EC
+{
+    public class SubmissionEC
+    {
+        public List<Submission> GetSubmissions(int id)
+        {
+            var Course = FakeDatabaseContext.SpringCourses.FirstOrDefault(x => x.Id == id);
+            if (Course == null)
+            {
+                Course = FakeDatabaseContext.FallCourses.FirstOrDefault(x => x.Id == id);
+            }
+            if (Course == null)
+            {
+                Course = FakeDatabaseContext.SummerCourses.FirstOrDefault(x => x.Id == id);
+            }
+            if (Course != null)
+            {
+                return Course.Submissions;
+            }
+            else
+            {
+                return new List<Submission>();
+            }
+        }
+
+        public void AddOrUpdateSubmission(Course c)
+        {
+            var Course = FakeDatabaseContext.SpringCourses.FirstOrDefault(x => x.Id == c.Id);
+            if (Course == null)
+            {
+                Course = FakeDatabaseContext.FallCourses.FirstOrDefault(x => x.Id == c.Id);
+            }
+            if (Course == null)
+            {
+                Course = FakeDatabaseContext.SummerCourses.FirstOrDefault(x => x.Id == c.Id);
+            }
+            if (Course != null)
+            {
+                var Submission = c.SelectedSubmission;
+                if (Submission.Id > 0)
+                {
+                    var itemToUpdate = Course.Submissions.FirstOrDefault(x => x.Id == Submission.Id);
+                    if (itemToUpdate != null)
+                    {
+                       itemToUpdate.Grade = Submission.Grade;
+                    }
+                }
+                else
+                {
+                    int lastId;
+                    if (Course.Submissions.Count > 0)
+                    {
+                        lastId = Course.Submissions.Select(x => x.Id).Max();
+                    }
+                    else
+                    {
+                        lastId = 0;
+                    }
+                    Submission.Id = ++lastId;
+                    Course.Submissions.Add(Submission);
+                }
+            }
+        }
+    }
+}
